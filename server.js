0cd24ba28b6cdb11bcef2865696b48cddfced9a7
druloloy <dru.loloy@gmail.com>
2021-04-08 20:49:01 +0800

    bugs fixed

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use('/api/students', require('./routes/student.route'));
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'client','build')));
 
-    app.get('/', (req,res)=>{
-        res.sendFile(path.join(__dirname, 'client','build','index.html'));
+    app.get('/*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
     })  
     
 }else{
