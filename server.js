6f153f59ccb90c05be131d2e34c85d34aee530ed
druloloy <dru.loloy@gmail.com>
2021-04-08 21:29:51 +0800

    bugs fixed

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'client','build')));
 
     app.get('/*', (req,res)=>{
-        const filePath = path.resolve(__dirname, "client", "build", "index.html");
+        const filePath = path.join(__dirname, "client", "build", "index.html");
         res.sendFile(filePath);
         console.log(filePath);
     })  
